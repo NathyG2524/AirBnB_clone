0f471b1dd357fd8f07642b6a94ec8c3766160ac0
NathyG2524 <Nathygirma2524@gmail.com>
2022-01-30 09:59:02 -0800

    fix bug on reload and save

--- models/base_model.py
+++ models/base_model.py
@@ -59,7 +59,7 @@ class BaseModel:
         """
         Returns dictionary representation of Base Instance
         """
-        new_dict = self.__dict__
+        new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
