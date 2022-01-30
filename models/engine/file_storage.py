6d31c2bcc0cb2fd3928f9e675a4fb4b5df60bf75
NathyG2524 <Nathygirma2524@gmail.com>
2022-01-30 10:13:04 -0800

    console 1.0

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,6 +12,9 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
 
 class FileStorage:
     """
